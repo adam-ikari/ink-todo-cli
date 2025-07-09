8bae961104a444e519e5e4c4debc822f9bc1a5be
adam <chenzd123456@gmail.com>
2025-07-09 19:54:31 +0800

    使用babel编译tsx

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,4 +1,20 @@
 import esbuild from 'esbuild';
+import { transform } from '@babel/core';
+import fs from 'fs';
+
+const babelPlugin = {
+  name: 'babel',
+  setup(build) {
+    build.onLoad({ filter: /\.(jsx|tsx)$/ }, async (args) => {
+      const source = await fs.promises.readFile(args.path, 'utf8');
+      const result = await transform(source, {
+        filename: args.path,
+        presets: ['@babel/preset-react', '@babel/preset-typescript']
+      });
+      return { contents: result.code, loader: 'js' };
+    });
+  },
+};
 
 const config = {
   entryPoints: ['src/cli.tsx'],
@@ -11,6 +27,7 @@ const config = {
   outfile: 'dist/cli.js',
   format: 'esm',
   external: ['react', 'ink', 'zustand', 'chalk', 'meow'],
+  plugins: [babelPlugin],
 };
 
 esbuild.build(config).catch(() => process.exit(1));
